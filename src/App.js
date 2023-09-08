ca26d5d316d6eb3635b0e4b2ecc73649371457a5
ShengBin-101 <shengbin.chan@gmail.com>
2023-09-09 01:49:17 +0800

    Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
-
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
+    <div className="App">
+      <Header />
       <About />
       <Projects />
-      <Skills />
-      <Testimonials />
       <Contact />
-    </main>
+    </div>
   );
 }
 
